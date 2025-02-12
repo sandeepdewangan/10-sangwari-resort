b2d7c1a32bdfece18d58be5298758271361ae0a4
Sandeep <sandeepdewangan7@gmail.com>
2025-02-12 07:00:23 +0530

    Supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins";
+
 const Cabins = () => {
+  useEffect(function () {
+    getCabins().then((d) => console.log(d));
+  }, []);
   return <div>Cabins</div>;
 };
 
---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log("Cabin cannot be loaded..." + error);
+  }
+
+  return data;
+}
