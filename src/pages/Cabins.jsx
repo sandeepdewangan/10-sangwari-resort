47ba8347a4706874fa1686228cc955457046819f
Sandeep <sandeepdewangan7@gmail.com>
2025-02-12 07:01:11 +0530

    -

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,4 @@
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins";
-
 const Cabins = () => {
-  useEffect(function () {
-    getCabins().then((d) => console.log(d));
-  }, []);
   return <div>Cabins</div>;
 };
 
