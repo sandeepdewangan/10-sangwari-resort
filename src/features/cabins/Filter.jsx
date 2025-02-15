65678d0b25c9cb4ad90662ce56e82f077ae62636
Sandeep <sandeepdewangan7@gmail.com>
2025-02-15 12:29:19 +0530

    Re-usability <Filter /> component.

--- src/features/cabins/Filter.jsx
+++ src/features/cabins/Filter.jsx
@@ -3,30 +3,26 @@ import { useSearchParams } from "react-router-dom";
 const labelStyle =
   "bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-100 hover:bg-blue-600 cursor-pointer";
 
-const Filter = () => {
+// this filter buttons can be used with any tables like, bookings, users etc.
+// now it become re-usable component.
+const Filter = ({ filterField, options }) => {
   const [searchParam, setSearchParam] = useSearchParams();
 
   function handleOnClick(value) {
-    searchParam.set("discount", value);
+    searchParam.set(filterField, value);
     setSearchParam(searchParam);
   }
   return (
     <>
-      <span className={labelStyle} onClick={() => handleOnClick("all")}>
-        All
-      </span>
-      <span
-        className={labelStyle}
-        onClick={() => handleOnClick("with-discounts")}
-      >
-        With Discounts
-      </span>
-      <span
-        className={labelStyle}
-        onClick={() => handleOnClick("no-discounts")}
-      >
-        No Discounts
-      </span>
+      {options.map((filterButton) => (
+        <span
+          className={labelStyle}
+          onClick={() => handleOnClick(filterButton.value)}
+          key={filterButton.value}
+        >
+          {filterButton.label}
+        </span>
+      ))}
     </>
   );
 };
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -6,7 +6,14 @@ const Cabins = () => {
     <div className="mx-25">
       <span className="text-2xl">Cabins</span>
       <div>
-        <Filter />
+        <Filter
+          filterField={"discount"}
+          options={[
+            { label: "All", value: "all" },
+            { label: "With Discounts", value: "with-discounts" },
+            { label: "No Discounts", value: "no-discounts" },
+          ]}
+        />
       </div>
 
       <div className="pt-2">
